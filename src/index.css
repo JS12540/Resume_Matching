c8b0106ed1bc68b9db8f1fbf34327a48f36b6462
aakashvanmali45 <aakashvanmali05@gmail.com>
2023-09-24 19:43:36 +0530

    Major code fixes

---
+++ src/index.css
@@ -0,0 +1,16 @@
+.dropzone {
+  border: 2px dashed #cccccc;
+  background: #f9f9f9;
+  text-align: center;
+  padding: 30px;
+  cursor: pointer;
+}
+
+.dropzone p {
+  font-size: 16px;
+  margin: 0;
+}
+
+.dropzone:hover {
+  border-color: #007bff;
+}
