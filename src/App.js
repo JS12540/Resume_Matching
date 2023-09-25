39a76e41a6c66ee8eee6f4f2fecfd85fcf3e9a5f
JS12540 <jayshah0726@gmail.com>
2023-09-25 18:10:39 +0530

    major fixes

--- src/App.js
+++ src/App.js
@@ -1,94 +1,77 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Container, Form, Button, Alert } from 'react-bootstrap';
-import Dropzone from 'react-dropzone';
-import './index.css'
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [jobDescription, setJobDescription] = useState('');
-  const [resumeFile, setResumeFile] = useState(null);
-  const [similarity, setSimilarity] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [jobDescription, setJobDescription] = useState("");
+  const [resumeFiles, setResumeFiles] = useState([]);
+  const [similarities, setSimilarities] = useState([]);
+  const [error, setError] = useState("");
 
-  const handleJobDescriptionChange = (e) => {
-    setJobDescription(e.target.value);
-  };
-
-  const handleFileDrop = (acceptedFiles) => {
-    setResumeFile(acceptedFiles[0]);
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setResumeFiles(files);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Check if both job description and resume file are provided
-    if (!jobDescription || !resumeFile) {
-      setErrorMessage('Please provide both a job description and a resume file.');
-      return;
-    }
-
-    // Create a FormData object for the file upload
     const formData = new FormData();
-    formData.append('job_description', jobDescription);
-    formData.append('resume_file', resumeFile);
+    formData.append("job_description", jobDescription);
+    resumeFiles.forEach((file, index) => {
+      formData.append(`resume_file_${index}`, file);
+    });
 
     try {
-      // Send the job description and resume file to the Flask backend
-      const response = await axios.post('/resume_matcher', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
+      const response = await axios.post("/resume_matcher", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
       });
 
-      setSimilarity(response.data.similarity.toFixed(2));
-      setErrorMessage('');
-    } catch (error) {
-      console.error(error);
-      setErrorMessage('An error occurred while processing your request.');
+      setSimilarities(response.data.similarities);
+      setError("");
+    } catch (err) {
+      setError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <Container className="mt-5">
+    <div className="App">
       <h1>Resume Matcher</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="jobDescription">
-          <Form.Label>Job Description</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={4}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Job Description:</label>
+          <textarea
             value={jobDescription}
-            onChange={handleJobDescriptionChange}
+            onChange={(e) => setJobDescription(e.target.value)}
+            required
           />
-        </Form.Group>
-
-        <Form.Group controlId="resumeFile">
-          <Form.Label>Upload Resume</Form.Label>
-          <Dropzone onDrop={handleFileDrop}>
-            {({ getRootProps, getInputProps }) => (
-              <div {...getRootProps()} className="dropzone">
-                <input {...getInputProps()} />
-                <p>Drag &amp; drop a resume file here, or click to select one</p>
-              </div>
-            )}
-          </Dropzone>
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Match Resume
-        </Button>
-      </Form>
-
-      {similarity !== null && (
-        <Alert className="mt-3" variant="success">
-          Matching Score: {similarity}
-        </Alert>
+        </div>
+        <div className="form-group">
+          <label>Upload Resumes:</label>
+          <input
+            type="file"
+            accept=".pdf"
+            multiple
+            onChange={handleFileChange}
+            required
+          />
+        </div>
+        <button type="submit">Match Resumes</button>
+      </form>
+      {error && <p className="error">{error}</p>}
+      {similarities.length > 0 && (
+        <div className="results">
+          <h2>Similarities:</h2>
+          <ul>
+            {similarities.map((similarity, index) => (
+              <li key={index}>{`Resume ${index + 1}: ${similarity.toFixed(
+                2
+              )}`}</li>
+            ))}
+          </ul>
+        </div>
       )}
-
-      {errorMessage && (
-        <Alert className="mt-3" variant="danger">
-          {errorMessage}
-        </Alert>
-      )}
-    </Container>
+    </div>
   );
 }
 
